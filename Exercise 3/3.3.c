44ae428827af23ab9aba2965b6ad4352ac4c3424
oarts12 <161311857+oarts12@users.noreply.github.com>
2024-10-23 17:11:20 +0300

    Update 3.3.c

--- Exercise 3/3.3.c
+++ Exercise 3/3.3.c
@@ -1,83 +1,95 @@
-/*
- * main.c
- *
- * Created: 10/20/2024 12:46:48 PM
- *  Author: Konstantinos
- */ 
-
-#define F_CPU 1600000UL
+#define F_CPU 16000000UL
 #include "avr/io.h"
 #include <util/delay.h>
+#include <stdbool.h>
 
 void adc_init(void) {
-	// Configure ADC
-	// Set reference voltage to AVcc (with external capacitor on AREF)
-	ADMUX |= (1 << REFS0);
-	
-	//Set ADC left adjusted
-	ADMUX |= (1 << ADLAR);
-	
-	// Set input channel to ADC1 (adjust according to your pin configuration)
-	ADMUX &= ~(1 << MUX0);  //0000 = ADC0
-	
-	// Set ADC prescaler to 128 (for 16 MHz clock, ADC clock = 16MHz / 128 = 125 kHz)
-	// This ensures the ADC clock is within the recommended range (50 kHz - 200 kHz)
-	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+	// Initialize ADC
+	// Set reference voltage to AVcc (with external capacitor at AREF pin)
+	ADMUX = (1 << REFS0);
+   
+	// Set ADC pre-scaler to 128 (for 16 MHz clock, ADC clock = 16MHz /128 = 125 kHz - within the range)
+	ADCSRA = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 	
 	// Enable the ADC
 	ADCSRA |= (1 << ADEN);
 }
 
 uint16_t adc_read(void) {
-	// Start the conversion
+	// Set ADSC flag of ADSCRA
 	ADCSRA |= (1 << ADSC);
 	
-	// Wait for the conversion to complete (ADSC becomes '0' when the conversion is done)
+	// Wait for ADSC flag to become '0'
 	while (ADCSRA & (1 << ADSC));
-	
-	// Return the 10-bit ADC result
-	return ADC;
+    
+    
+	/* if we want to be 1000% certain use the following code
+    uint16_t result = 0x0000, dummy = 0x0000;
+    result = result | ADCL;
+    dummy = dummy | ADCH;
+    dummy = dummy << 8;
+    result = result | dummy;
+	return result;
+     */ 
+    
+    //Return 10-bit result
+    return ADC;
 }
 
 int main(){	
-	int DC_VALUES_1[14];		//Create table for dc_values
+	int DC_VALUES[14];		//Create table for dc_values
 
 	for (int i = 0; i < 13; i++){		//set up the DC_VALUE table
-		DC_VALUES_1[i] = (8 + i*20);
+		DC_VALUES[i] = (8 + i*20);
 	}
+    adc_init();			//Initialize ADC
 
-	/*	set TMR1A in fast PWM 8 bit mode with non inverted output
-	and prescale  = 256
-	*/
+
+	// set fast non-inverted PWM 8 bit and pre-scale  = 1
 	TCCR1A = (1 << WGM10) | (1 << COM1A1);
-	TCCR1B = (1 << WGM12) | (1 << CS12);
+	TCCR1B = (1 << WGM12) | (1<<CS10);
 	
-	DDRB |= 0b00011111;
-	DDRD |= 0b00011111;		//PD4-PD0 output, PD5-PD7 input				
+	DDRB = 0xFF;        //PORTB as output
+    DDRC = 0x00;        //PORTC is left as input
+	DDRD = 0x00;		//PORTD as input				
 	int index = 7;			//Duty Cycle =  50%
+    OCR1AL = DC_VALUES[index];
+    unsigned char mode1 = 1;
+    unsigned char mode2 = 1;
+    bool flag = true;
 
 	while(1){		
-		/*CHECK IF PD6 WAS PRESSED ENTER MOPDE 1*/
-	
-			/*Increase or decrease the Duty Cycle
-			based on which button is pressed*/												
-			if(PORTD == 0x01){
-				index++;
-				if(index == 13){
-					continue;
-				}
-				OCR1AL = DC_VALUES_1[index];
-			}
-			if(PORTD == 0x02){
-				index--;
-				if(index == 0){
-					continue;
-				}			
-				OCR1AL = DC_VALUES_1[index];
-			}
-		/*CHECK IF PD7 WAS PRESSED ENTER MODE 2*/
-			adc_init();
-			OCR1AL = adc_read();
-			
-	}
+        // Set the desired mode
+        mode1 = PIND & 0x40;
+        mode2 = PIND & 0x80;
+        if (mode1 ==0) {
+            flag = true;
+            OCR1AL = DC_VALUES[index];
+        }
+        if (mode2 ==0) {
+            flag = false;            
+        }
+        if(flag) {
+            _delay_ms(100);
+            unsigned char pd1 = PIND & 0x02, pd2 = PIND & 0x04, check = PIND;
+        
+            //wait for the button to be un-pressed
+            while (!(check & (1 << PD1)) | !(check & (1<<PD2))) {
+                check = PIND;
+            }
+
+            
+            if((pd1 ==0) && index < 12){ // with PD1 we increase
+                index++;
+                OCR1AL = DC_VALUES[index];
+            }
+            if((pd2 ==0) && index > 0){ // with PD2 we decrease
+                index--;
+                OCR1AL = DC_VALUES[index];
+            }
+        }
+        else {
+            OCR1AL = adc_read() >> 2; 
+        }
+    }
 }
