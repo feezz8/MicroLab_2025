c06e2cf02fa3ce6a3089e3d8777d9ee7ad8b67f6
oarts12 <161311857+oarts12@users.noreply.github.com>
2024-11-03 12:48:40 +0200

    Create 5_3_main.c

---
+++ Exercise_5_3/5_3_main.c
@@ -0,0 +1,36 @@
+#include "53headers.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+int main() {
+    twi_init();
+    PCA9555_0_write(REG_CONFIGURATION_1, 0x00); //Set EXT_PORT1 as output
+    lcd_init();
+    char fezz1[] = "Konstantinos", fezz2[] = "Fezzos", oar1[] = "Odysseas", oar2[] = "Tsouknidas";    
+    while (1) {
+        lcd_clear_display();
+        for (int i=0; i<12; i++){
+            lcd_data(fezz1[i]);
+        }
+        lcd_change_line();
+        for (int i=0; i<6; i++){
+            lcd_data(fezz2[i]);
+        }
+    
+    
+        _delay_ms(5000);
+        // Now time for the next name
+        lcd_clear_display();
+        for (int i=0; i<8; i++) {
+            lcd_data(oar1[i]);
+        }
+        lcd_change_line();
+        for (int i=0; i<10; i++){
+            lcd_data(oar2[i]);            
+        }
+        _delay_ms(5000);
+        
+    }
+    return 0;
+}
