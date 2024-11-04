57bb811777385ab60c29291d26e9da20b5ffd784
oarts12 <161311857+oarts12@users.noreply.github.com>
2024-11-04 18:59:44 +0200

    Update 5.1.c

--- Exercise 5/Exercise 5.1/5.1.c
+++ Exercise 5/Exercise 5.1/5.1.c
@@ -8,12 +8,14 @@ int main() {
     DDRB = 0x00;
     //Set PORTD as output in order to connect PD2 and PD3 with IO0_0 and IO0_1
     DDRD = 0xFF;
+    //DDRC = 0xFF;
     unsigned char A,B,C,D, F0, F1, input, result;
     twi_init();
     PCA9555_0_write(REG_CONFIGURATION_0, 0x00); //Set EXT_PORT0 as output
     
     while(1) {
         input = PINB;
+   
         A = input & 0x01;
         B = (input & 0x02) >> 1;
         C = (input & 0x04) >> 2;
@@ -23,7 +25,7 @@ int main() {
         F1 = (A | B | C) & (B & (~D));
         F1 = (F1 & 0x01) << 1;
         result = F0 | F1;
-        //Maybe I need PCA Config output to take it to LEDs?
         PCA9555_0_write(REG_OUTPUT_0, result);
+        
     }
 }
