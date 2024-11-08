35d033bc38ec82b30f46da43f7e8f7be5f6e57fd
oarts12 <161311857+oarts12@users.noreply.github.com>
2024-11-08 18:05:49 +0200

    Update 5.2.main.c

--- Exercise 5/Exercise 5.2/5.2.main.c
+++ Exercise 5/Exercise 5.2/5.2.main.c
@@ -7,29 +7,35 @@ int main() {
     twi_init();
     //Set Port Expander 0 as output
     PCA9555_0_write(REG_CONFIGURATION_0,0x00);
-    //Set Port Expander 1 [3:0] as output and [7:4] as input
-    PCA9555_0_write(REG_CONFIGURATION_1,0xF0);
+    //Set Port Expander 1 [3:0] as output ((and [7:4] as input))
+    PCA9555_0_write(REG_CONFIGURATION_1,0xFE);
     uint8_t input;
-    
+    //DDRD = 0xFF;
+    PCA9555_0_write(REG_OUTPUT_1,0x00); //maybe
+
     
     while(1)
     {
+        //PCA9555_0_write(REG_OUTPUT_0, 0xFF);
         input = PCA9555_0_read(REG_INPUT_1);
-        if(input & 0x10) //*
+        
+        
+        if((input & 0x10) == 0x00) //*
+            // WE ALWAYS GO HERE
         {
-            PCA9555_0_write(REG_OUTPUT_0, 0x10);
+            PCA9555_0_write(REG_OUTPUT_0, 0x01);
         }
-        else if(input & 0x20) // 0
+        else if((input & 0x20) == 0x00) // 0
         {
-            PCA9555_0_write(REG_OUTPUT_0, 0x20);
+            PCA9555_0_write(REG_OUTPUT_0, 0x02);
         }
-        else if(input & 0x40) // #
+        else if((input & 0x40) == 0x00) // #
         {
-            PCA9555_0_write(REG_OUTPUT_0, 0x40);
+            PCA9555_0_write(REG_OUTPUT_0, 0x04);
         }
-        else if(input & 0x80) // D
+        else if((input & 0x80) == 0x00) // D
         {
-            PCA9555_0_write(REG_OUTPUT_0, 0x80);
+            PCA9555_0_write(REG_OUTPUT_0, 0x08);
         }
         else
         {
